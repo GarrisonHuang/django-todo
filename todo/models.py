d242329e01f5974ad17e73d54a0d9ebe8dd84f05
GarrisonHuang <conquerkwu@hotmail.com>
2025-04-16 12:28:52 +0800

    完成user 各自的todolist

--- todo/models.py
+++ todo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -6,8 +7,9 @@ class Todo(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    date_completed = models.DateTimeField(blank=True, null=True)
+    date_completed = models.BooleanField(blank=True, null=True)
     important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.id}-{self.title}-{self.created}"
